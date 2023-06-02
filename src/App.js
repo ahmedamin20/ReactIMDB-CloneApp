65ae1bc1a61e26e43b3a7a467bd8acb10cfd1137
ahmedamin20 <ahmedamin01147@hotmail>
2023-06-02 18:15:03 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './Components/Header/Header';
+import Home from './pages/home/home';
+import MovieList from './Components/movieList/movieList';
+import Movie from './pages/movieDetail/movie';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<h1 style={{ margin: "1rem ", textAlign: "center" }}>Error Page!!<br /><br />Sorry Page Not found</h1>}></Route>
+        </Routes>
+      </Router>
+    </div >
+  );
+}
+
+export default App;
---
+++ src/Components/card/card.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "./card.css";
+import { Link } from "react-router-dom";
+import "react-loading-skeleton/dist/skeleton.css";
+const Card = ({ movie }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="cards">
+          <SkeletonTheme color="#000000" highLightColor="#6b6b6b44">
+            <Skeleton
+              height={300}
+              duration={2}
+              animation="wave"
+              sx={{ bgcolor: "grey.900" }}
+            />
+          </SkeletonTheme>
+        </div>
+      ) : (
+        <Link
+          to={`/movie/${movie.id}`}
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          <div className="cards">
+            <img
+              className="cards__img"
+              src={`https://image.tmdb.org/t/p/original${
+                movie ? movie.poster_path : ""
+              }`}
+            />
+            <div className="cards__overlay">
+              <div className="card__title">
+                {movie ? movie.original_title : ""}
+              </div>
+              <div className="card__runtime">
+                {movie ? movie.release_date : ""}
+                <span className="card__rating">
+                  {movie ? movie.vote_average : ""}
+                  <i className="fas fa-star" />
+                </span>
+              </div>
+              <div className="card__description">
+                {movie ? movie.overview.slice(0, 118) + "..." : ""}
+              </div>
+            </div>
+          </div>
+        </Link>
+      )}
+    </>
+  );
+};
+export default Card;
